d227c0652fd13804390ddfd5600e5d9d145cfe5d
Sungho Park <43461389+psh320@users.noreply.github.com>
2023-03-16 14:57:29 +0800

    feat: add util functions

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,6 @@
 import moment from "moment";
 import { Quote } from "../components/Auto/type";
+import { Wallet } from "../stocker-core/sdk/Types/Account";
 
 export const parsePriceList = (data: Quote[]): number[] => {
   const parsedPriceArray: number[] = [];
@@ -23,3 +24,18 @@ export function convertTime(timestamp: number) {
   const date = new Date(timestamp);
   return moment(date).format("DD/MM/YYYY hh:mm:ss");
 }
+
+export const parseCoinList = (wallets: Wallet[]): Wallet[] => {
+  const resultList = wallets.filter((wallet) => wallet.amount > 0);
+  return resultList;
+};
+
+export const compare = (a: any, b: any) => {
+  if (a.timestamp > b.timestamp) {
+    return -1;
+  }
+  if (a.timestamp < b.timestamp) {
+    return 1;
+  }
+  return 0;
+};
