318e2912e3da2e7b6171c12b10b6426d435b28b3
Sungho Park <43461389+psh320@users.noreply.github.com>
2023-03-24 01:25:17 +0800

    feat: add mapping function

--- constant/CoinData.ts
+++ constant/CoinData.ts
@@ -1,16 +1,3 @@
-// export const coinList = [
-//   { symbol: "btcusdt", code: "btc", name: "Bitcoin" },
-//   { symbol: "ethusdt", code: "eth", name: "Etherium" },
-//   { symbol: "xrpusdt", code: "xrp", name: "Ripple" },
-//   { symbol: "solusdt", code: "sol", name: "Solana" },
-//   { symbol: "bnbusdt", code: "bnb", name: "Binancecoin" },
-//   { symbol: "ltcusdt", code: "ltc", name: "Litecoin" },
-//   { symbol: "aptusdt", code: "apt", name: "Aptos" },
-//   { symbol: "shibusdt", code: "shib", name: "Shiba Inu" },
-//   { symbol: "enjusdt", code: "enj", name: "Enjin" },
-//   { symbol: "maticusdt", code: "matic", name: "Polygon" },
-// ];
-
 export const coinList = [
   { symbol: "BTCUSDT", code: "btc", name: "Bitcoin" },
   { symbol: "ETHUSDT", code: "eth", name: "Etherium" },
@@ -24,7 +11,36 @@ export const coinList = [
   { symbol: "MATICUSDT", code: "matic", name: "Polygon" },
 ];
 
-export const getCoinName = (symbol: string) => {
-  const result = coinList.filter((item) => item.symbol === symbol);
-  return result[0].name;
+type CoinInfo = {
+  code: string;
+  name: string;
 };
+
+export function getCoinInfo(symbol: string): CoinInfo | undefined {
+  switch (symbol) {
+    case "USD":
+      return { code: "usd", name: "US Dollar" };
+    case "BTCUSDT":
+      return { code: "btc", name: "Bitcoin" };
+    case "ETHUSDT":
+      return { code: "eth", name: "Ethereum" };
+    case "XRPUSDT":
+      return { code: "xrp", name: "Ripple" };
+    case "SOLUSDT":
+      return { code: "sol", name: "Solana" };
+    case "BNBUSDT":
+      return { code: "bnb", name: "BinanceCoin" };
+    case "LTCUSDT":
+      return { code: "ltc", name: "Litecoin" };
+    case "ADAUSDT":
+      return { code: "ada", name: "Ada" };
+    case "DOTUSDT":
+      return { code: "dot", name: "Polkadot" };
+    case "ENJUSDT":
+      return { code: "enj", name: "Enjin" };
+    case "MATICUSDT":
+      return { code: "matic", name: "Polygon" };
+    default:
+      return undefined; // Symbol not found
+  }
+}
