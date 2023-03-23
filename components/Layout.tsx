a22c218aa14dcd7032034c8c59ab4d7daa9741b7
Sungho Park <43461389+psh320@users.noreply.github.com>
2023-03-24 01:11:20 +0800

    style: mobile maximum width to 450

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -35,7 +35,7 @@ const Layout = ({ children }: LayoutProp) => {
                 sm: 600,
               },
               maxWidth: {
-                xs: 350,
+                xs: 450,
                 sm: 600,
               },
             },
